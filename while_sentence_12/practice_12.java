38fbd09fe371f7afc645e924cbd1072f6a89c4ac
suemoto <suemoto.3456@gmail.com>
2023-04-28 17:26:06 +0900

    while型反復処理

--- while_sentence_12/practice_12.java
+++ while_sentence_12/practice_12.java
@@ -15,21 +15,6 @@
 package while_sentence_12;
 
 public class practice_12 {
-
-	/*
-	問題．while文を使用し、以下のプログラムを作成してください。
-		・1～100を出力
-		・奇数の数字に「〇は奇数です」と出力
-		・偶数の数字に「○は偶数です」と出力
-		  出力結果  1は奇数です
-					2は偶数です
-					3は奇数です
-					4は偶数です
-						・
-						・
-						・
-					100は偶数です
-*/
 	
 	public static void main(String[] args) {
 		
