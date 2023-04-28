12c39dd6968481b88816e4210c6f3c2e1dee86f9
suemoto <suemoto.3456@gmail.com>
2023-04-28 17:19:02 +0900

    配列2

--- array2_14/practice_14.java
+++ array2_14/practice_14.java
@@ -23,26 +23,6 @@ package array2_14;
 
 public class practice_14 {
 	
-//	string型の配列を作成し、アルファベットa～jまでを格納してください。
-//	brake文を使用し、a～dまで表示してください。
-//	
-//	 出力結果：a
-//	           b
-//	           c
-//	           d
-//	          
-//	cotinue文を使用し、i以外を表示してください。
-//	
-//	 出力結果：a
-//	           b
-//	           c
-//	           d
-//	           e
-//	           f
-//	           g
-//	           h
-//	           j
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		//String型配列alphabetを宣言,値a～jを代入
@@ -57,24 +37,24 @@ public class practice_14 {
 			//String型変数spaceを宣言、,文字"出力結果"以降の行間を代入
 			String space = "         ";
 			
-			for(int y = 0; y < alphabet.length; y++) {
+			for(int num = 0; num < alphabet.length; num++) {
 				
 				//配列alphabetの値dまで出力
-				if(alphabet[y] == "e" && x == 0) {
+				if(alphabet[num] == "e" && x == 0) {
 					break;
 				}
 				
 				//配列alphabetの値i以外を出力
-				if(alphabet[y] == "i" && x == 1) {
+				if(alphabet[num] == "i" && x == 1) {
 					continue;
 				}
 				
-				if(y >= 1) {
+				if(num >= 1) {
 					//変数spaceを出力
 					System.out.print(space);
 				}
 				//配列alphabetを出力
-				System.out.println(alphabet[y]);
+				System.out.println(alphabet[num]);
 			}
 			//出力結果を改行
 			System.out.println("\r\n");
