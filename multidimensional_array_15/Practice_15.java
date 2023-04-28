bf21108b9f79d4ee65633bce3bf399aa27debf7e
suemoto <suemoto.3456@gmail.com>
2023-04-28 17:29:25 +0900

    多次元配列

--- multidimensional_array_15/Practice_15.java
+++ multidimensional_array_15/Practice_15.java
@@ -15,18 +15,7 @@ package multidimensional_array_15;
 import java.util.Arrays;
 
 public class Practice_15 {
-	/*
-	①配列int test[][]に以下の成績を格納し、表示してください。
-		出力結果：生徒1：国語64点、数学73点、英語69点
-				  生徒2：国語58点、数学81点、英語75点
-				  生徒3：国語86点、数学68点、英語79点
-				  生徒4：国語72点、数学55点、英語80点
-		
-	②それぞれの教科の平均点を表示してください。
-		出力結果：国語の平均点は○○点です。
-				  数学の平均点は○○点です。
-				  英語の平均点は○○点です。
-	*/
+
 	public static void main(String[] args) {
 		
 		//多次元配列testを宣言
