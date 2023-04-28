c5904c2659602eb772f6ca3525ced6abed7674e0
suemoto <suemoto.3456@gmail.com>
2023-04-28 17:24:39 +0900

    switch型条件分岐

--- practice_switch_11/sample_11.java
+++ practice_switch_11/sample_11.java
@@ -16,21 +16,6 @@ package practice_switch_11;
 import java.util.Random;
 
 public class sample_11 {
-
-	/*
-	問題．switch文を使用し、以下のプログラムを作成してください。
-			変数に数値を代入
-			
-		①2の場合は、「（変数）月は28日です。」
-			出力結果：2月は28日です。
-			
-		②4，6，9，11の場合は、「（変数）月は30日です。」
-			出力例：4月は30日です。
-			
-		③その他の場合は、「（変数）月は31日です。」
-			出力例：1月は31日です。
-*/
-	
 	
 	public static void main(String[] args) {
 
