1c92d02dd92c86df2136855b3d28f965b69ce5ba
suemoto <suemoto.3456@gmail.com>
2023-05-01 12:13:58 +0900

    配列1

--- array1_13/Practice1_13.java
+++ array1_13/Practice1_13.java
@@ -17,13 +17,13 @@ public class Practice1_13 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		//int型配列aを宣言
+		// int型配列aを宣言
 		int[] a = {1, 2, 3, 4, 5};
 		
-		//変数iを宣言、forを用いてiの値が4になるまで出力
+		// 変数iを宣言、forを用いてiの値が4になるまで出力
 		for(int i = 0; i < 5; i++) {
 			
-			//配列aを変数iを用いて出力
+			// 配列aを変数iを用いて出力
 			System.out.println(a[i]);
 		}
 		
--- array1_13/Practice2_13.java
+++ array1_13/Practice2_13.java
@@ -18,28 +18,26 @@ public class Practice2_13 {
 	
 	public static void main(String[] args) {
 		
-		//int型配列aを宣言
+		// int型配列aを宣言
 		int[] a = {1, 2, 3, 4, 5};
 		
-		//参照型配列bを宣言、格納数3
+		// 参照型配列bを宣言、格納数3
 		Point[] b = new Point[3];
 		
-		//配列bの要素数を出力
-		System.out.println("出力結果:" + b.length);
+		// 配列bの要素数を出力
+		System.out.println("② 参照型配列 b を作成し、3を表示してください。");
+		System.out.println("出力結果:" + b.length + "\n");
 		
-		//改行を出力
-		System.out.println("");
-		
-		//配列a[3]に値10を代入
+		// 配列a[3]に値10を代入
 		a[3] = 10;
 		
-		//配列a[3]を出力
-		System.out.println("出力結果:"+a[3]);
+		// 配列a[3]を出力
+		System.out.println("③配列a[3]を10で上書きし表示してください。");
+		System.out.println("出力結果:"+a[3] + "\n");
 		
-		//改行を出力
-		System.out.println("");
-		
-		//配列aの要素数を出力
+		// 配列aの要素数を出力
+		System.out.println("④配列aの要素数を「配列aの要素数は、○です。」");
+		System.out.println("という形で表示してください。");
 		System.out.println("出力結果:配列aの要素数は、" +  a.length + "です。");
 	}
 
