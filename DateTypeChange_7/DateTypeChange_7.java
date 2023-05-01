81bb8fc9dff164551f6fa408461ddcee9bba83a9
suemoto <suemoto.3456@gmail.com>
2023-05-01 11:10:45 +0900

    型変換

--- DateTypeChange_7/DateTypeChange_7.java
+++ DateTypeChange_7/DateTypeChange_7.java
@@ -7,68 +7,94 @@ public class DateTypeChange_7 {
 //		以下のプログラムを作成してください。
 //		①int型変数 i に 3 を代入する。
 		
-		//int型変数iを宣言し値3を代入
+		// int型変数iを宣言し値3を代入
 		int i = 3;
 //		②double型変数 d に i を代入する。
 		
-		//double型変数dを宣言しiを代入
+		// double型変数dを宣言しiを代入
 		double d = i;
 //		③変数 i と変数 d の値をそれぞれコンソールに表示する。
 		
-		//変数iと変数dを出力
-		System.out.println(i);
-		System.out.println(d);
+		// 変数iと変数dを出力
+		
+		System.out.println("問1③: 変数 i と変数 d の値をそれぞれコンソールに表示する。\n");
+		System.out.println("変数 i");
+		System.out.println("出力結果:" + i + "\n");
+		System.out.println("変数 d");
+		System.out.println("出力結果: " + d + "\n");
+	
 //		問２）
 //		以下のプログラムを作成してください。
 //		①double型変数 pi に数値「3.14」を代入して下さい。。
 		
-		//double型変数piを宣言し値3.14を代入
+		// double型変数piを宣言し値3.14を代入
 		double pi = 3.14;
 //		②変数 pi に「3」を掛けた値をコンソールに出力して下さい。
 		
-		//変数piに値3を掛けて出力
-		System.out.println(pi * 3);
+		// 変数piに値3を掛けて出力
+		System.out.println("問2➁: 変数 pi に「3」を掛けた値をコンソールに出力して下さい。\n");
+		System.out.println("変数 pi × 3");
+		System.out.println("出力結果: " + pi * 3 + "\n");
 //		④int型の変数 integer を宣言し、変数 pi を int型に型変換しコンソールに出力して下さい。
 		
-		//int型変数integerを宣言、double型変数piをintに型変換して代入
+		// int型変数integerを宣言、double型変数piをintに型変換して代入
 		int integer = (int)pi;
 		
-		//変数integerを出力
-		System.out.println(integer);
+		// 変数integerを出力
+		System.out.println("問2④: int型の変数 integer を宣言し、変数 pi を");
+		System.out.println("int型に型変換しコンソールに出力して下さい。\n");
+		System.out.println("変数 integer");
+		System.out.println("出力結果: " + integer + "\n");
 //		問３）
 //		①double型の変数を定義し、「2 + 6.0」を代入して下さい。
 //		　double型の変数をコンソールに出力して下さい。
 		
-		//double型変数sumを宣言、2 + 6.0を代入
+		// double型変数sumを宣言、2 + 6.0を代入
 		double sum = 2 + 6.0;
 		
-		//変数sを出力
-		System.out.println(sum);
+		// 変数sumを出力
+		System.out.println("問3①: double型の変数を定義し、「2 + 6.0」を代入して下さい。");
+		System.out.println("double型の変数をコンソールに出力して下さい。\n");
+		System.out.println("double型の変数 sum");
+		System.out.println("出力結果: " + sum + "\n");
 //		②int型の変数を定義し、double型の変数をint型に型変換して代入下さい。
 //		　int型の変数をコンソールに出力して下さい。
 		
-		//int型変数sum2を宣言、double型変数sum2をintに型変換して代入
-		int sum2 = (int)sum;
-		//変数tを出力
-		System.out.println(sum2);
+		// int型変数 intSum を宣言、double型変数 intSum をintに型変換して代入
+		int intSum = (int)sum;
+		
+		// 変数sum2を出力
+		System.out.println("問3➁: int型の変数を定義し、");
+		System.out.println("double型の変数をint型に型変換して代入下さい。");
+		System.out.println("int型の変数をコンソールに出力して下さい。\n");
+		System.out.println("int型の変数 intSum2");
+		System.out.println("出力結果: "  + intSum + "\n");
 //		③String型の変数を定義し、int型の変数をString型に型変換して代入して下さい。
 //		　String型の変数をコンソールに出力して下さい。
 		
-		//String型変数str,int型変数numを宣言,変数numをStringに型変換して変数に代入
+		// String型変数str,int型変数numを宣言,変数numをStringに型変換して変数に代入
 		String str;
 		int num = 3;
 		str = String.valueOf(num);
 		
-		//変数uを出力
-		System.out.println(str);
+		// 変数strを出力
+		System.out.println("問3③: String型の変数を定義し、");
+		System.out.println("int型の変数をString型に型変換して代入して下さい。");;
+		System.out.println("String型の変数をコンソールに出力して下さい。\n");
+		System.out.println("String型の変数 str");
+		System.out.println("出力結果: " + str + "\n");
 //		④「②」で定義したint型変数に、String型の変数をint型に型変換して代入して下さい。
 //		　int型の変数をコンソールに出力して下さい。
 		
-		//String型変数sum3を宣言、int型変数sum2を型変換して変数sum3に代入
-		String sum3 = String.valueOf(sum2);
+		// String型変数sum3を宣言、int型変数sum2を型変換して変数sum3に代入
+		String strSum = String.valueOf(intSum);
 		
-		//変数wを出力
-		System.out.println(sum3);
+		// 変数sum3を出力
+		System.out.println("問3④ : 「②」で定義したint型変数に、");
+		System.out.println("String型の変数をint型に型変換して代入して下さい。");
+		System.out.println("String型の変数をコンソールに出力して下さい。\n");
+		System.out.println("String型の変数 strSum");
+		System.out.println("出力結果: " + strSum);
 
 	}
 }
