952d785eba3118691e9f06ff6b08e49e8016a90f
suemoto <suemoto.3456@gmail.com>
2023-05-01 12:18:18 +0900

    配列2

--- array2_14/practice_14.java
+++ array2_14/practice_14.java
@@ -25,38 +25,38 @@ public class practice_14 {
 	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		//String型配列alphabetを宣言,値a～jを代入
+		// String型配列alphabetを宣言,値a～jを代入
 		String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
 		
-		//配列alphabetをfor文,break分とcontinue分を用いて出力
+		// 配列alphabetをfor文,break分とcontinue分を用いて出力
 		for(int x = 0; x <= 1; x++) {
 			
-			//文字"出力結果"を出力
+			// 文字"出力結果"を出力
 			System.out.print("出力結果:");
 			
-			//String型変数spaceを宣言、,文字"出力結果"以降の行間を代入
+			// String型変数spaceを宣言、,文字"出力結果"以降の行間を代入
 			String space = "         ";
 			
 			for(int num = 0; num < alphabet.length; num++) {
 				
-				//配列alphabetの値dまで出力
+				// 配列alphabetの値dまで出力
 				if(alphabet[num] == "e" && x == 0) {
 					break;
 				}
 				
-				//配列alphabetの値i以外を出力
+				// 配列alphabetの値i以外を出力
 				if(alphabet[num] == "i" && x == 1) {
 					continue;
 				}
 				
 				if(num >= 1) {
-					//変数spaceを出力
+					// 変数spaceを出力
 					System.out.print(space);
 				}
-				//配列alphabetを出力
+				// 配列alphabetを出力
 				System.out.println(alphabet[num]);
 			}
-			//出力結果を改行
+			// 出力結果を改行
 			System.out.println("\r\n");
 		}
 		
