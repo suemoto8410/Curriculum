4799a101f7cf66e2f5a3e4a9811925c781b056ae
suemoto <suemoto.3456@gmail.com>
2023-07-31 16:13:52 +0900

    課題30

---
+++ curriculum30/Main.java
@@ -0,0 +1,54 @@
+package curriculum30;
+
+
+/*
+ * Q1：インスタンスフィールドを定義してください
+ * 
+ * Q2：コンストラクタを定義してください（下記それぞれの引数）
+ * 
+ * Q3：コンストラクタの中で各インスタンスフィールドに値をセットしてください
+ * 
+ * Q4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+ * 
+ * Q5：Main.javaの引数にweightの60を入れてください
+ * 
+ * Q6：インスタンスメソッド「bmi」を定義してください（戻り値：double）
+ * 
+ * Q7：bmiメソッドでインスタンスのBMIを返すようにしてください
+ * 
+ * Q8：インスタンスメソッド「print」を定義してください（戻り値：void）
+ * 
+ * Q9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+ * 
+ * Q10：人数の合計を「合計○人です」と出力してください。
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Q5：Main.javaの引数にweightの60を入れてください
+		
+		/**
+		 * 自己紹介プログラム
+		 * 身長は小数点第一(四捨五入), 体重小数点なし
+		 * @param 名前(フルネーム漢字), 年齢, 身長(m), 体重(kg)
+		 */
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		
+		// 出力
+		System.out.println(person1.name);
+		System.out.println(person1.age);
+		System.out.println(person1.height);
+		
+		// Q9
+		// 自己紹介文を出力
+		person1.print();
+		
+		// Q10
+		// 登録した人数を出力
+		person1.printCount();
+	
+	}
+
+}
---
+++ curriculum30/Person.java
@@ -0,0 +1,97 @@
+package curriculum30;
+
+public class Person {
+	
+	// Q1:インスタンスフィールドを定義してください
+	
+	/* 
+	 * フィールド名 データ型
+	 * name string
+	 * age int
+	 * height double
+	 */
+	
+	// 名前(漢字フルネーム)
+	public String name;
+	
+	// 年齢
+	public int age;
+	
+	// 身長(m小数点第一位)
+	public double height;
+	
+	// Q4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+	// 体重(kg小数点なし)
+	public double weight;
+	
+	// 自己紹介プログラムに登録した人数をカウント
+	public static int personCount;
+	
+	// Q2:コンストラクタを定義してください（下記それぞれの引数）
+	// name,age,height
+	
+	/**
+	 * 身長は小数点第一(四捨五入), 体重小数点なし
+	 * @param 名前(フルネーム漢字), 年齢, 身長(m), 体重(kg)
+	 */
+	public Person(String name, int age, double height, double weight)  {
+		
+		// Q3：コンストラクタの中で各インスタンスフィールドに値をセットしてください
+	
+		this.name = name;
+		
+		this.age = age;
+		
+		this.height = height;
+		
+		// Q4
+		this.weight = weight;
+		
+		// Q10
+		personCount++;
+	
+	}
+	
+	// Q6:インスタンスメソッド「bmi」を定義してください（戻り値：double）
+	// Q7:bmiメソッドでインスタンスのBMIを返すようにしてください
+	
+	/**
+	 * コンストラクタに渡された値でbmiを計算
+	 * @return bmi(小数第一位切り捨て)
+	 */
+	public double bmi() {
+		
+		double bmi = Math.floor(this.weight / (this.height * this.height));
+		
+		return bmi;
+		
+	}
+	
+	// Q8：インスタンスメソッド「print」を定義してください（戻り値：void）
+	// Q9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+	
+	// コンストラクタに渡された値で自己紹介文を出力
+	public void print() {
+		
+		String text = """
+				
+				名前は%sです
+				BMIは%.1fです
+				年は%d才です
+				
+				""";
+		
+		System.out.printf(text, this.name, bmi(), this.age);
+		
+	}
+	
+	// Q10：人数の合計を「合計○人です」と出力してください。
+	
+	// 登録された人数を出力
+	public void printCount() {
+		
+		System.out.println("合計" + personCount + "人です");
+	
+	}
+
+}
